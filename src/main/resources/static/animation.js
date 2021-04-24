670cbd847f8bf5c7aaf2e79dc43d62e58b3386c0
Benjamin Hartmann <extern.benjamin.hartmann@porscheinformatik.at>
2021-04-24 19:15:21 +0200

    first prototype of pie chart showing gaze counts per surface

--- src/main/resources/static/animation.js
+++ src/main/resources/static/animation.js
@@ -1,204 +1,104 @@
-var lineGenerator = d3.line()
-    .curve(d3.curveLinear);
-
-
-var xScale = d3.scaleLinear().range([0, 210]);
-var yScale = d3.scaleLinear().range([290, 0]);
-var length;
-var path;
-d3.csv("examples/gaze_cover.csv", function (d) {
-    var line = d3.line()
-        .x(function (d) {
-            return xScale(d.x_norm);
-        })
-        //        .x(function(d) { return d.x_norm })
-        .y(function (d) {
-            return yScale(d.y_norm);
+var url = 'https://gist.githubusercontent.com/d3byex/5a8267f90a0d215fcb3e/raw/2d30d7b9b85220395727311dcb94345ef64ca10c/multi_network.html';
+d3.json(url, function (error, data) {
+    var width = 960, height = 500;
+    var svg = d3.select('body')
+        .append('svg')
+        .attr({
+            width: width,
+            height: height
         });
 
-    var svg = d3.select('svg');
+    var force = d3.layout.force()
+        .size([width, height])
+        .linkDistance(1)
+        .charge(-4000)
+        .nodes(data.nodes)
+        .links(data.edges)
+        .start();
 
-    path = svg.append("path")
-        .datum(d)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr("d", line)
+    var linkTypes = d3.set(data.edges.map(function (d) {
+        return d.type;
+    })).values();
 
-
-    length = path.node().getTotalLength();
-
-    d3.select('svg')
-        .selectAll('circle')
-        .data(d)
+    svg.append("defs")
+        .selectAll("marker")
+        .data(linkTypes)
         .enter()
-        .append('circle')
-        .attr('cx', function (d) {
-            return xScale(d.x_norm);
+        .append("marker")
+        .attr({
+            id: function (d) { return d; },
+            viewBox: "0 -5 10 10",
+            refX: 20,
+            refY: -1.5,
+            markerWidth: 6,
+            markerHeight: 6,
+            orient: "auto"
         })
-        .attr('cy', function (d) {
-            return yScale(d.y_norm);
-        })
-        .attr('r', 3);
-});
-
-// This function will animate the path over and over again
-function animateLine() {
-    // Animate the path by setting the initial offset and dasharray and then transition the offset to 0
-    path.attr("stroke-dasharray", length + " " + length)
-        .attr("stroke-dashoffset", length)
-        .transition()
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        .duration(6000)
-        .on("end", () => setTimeout(repeat, 1000)); // this will repeat the animation after waiting 1 second
-
-    // Animate the dashoffset changes
-    text.transition()
-        .duration(6000)
-        .tween("text", function (t) {
-            const i = d3.interpolateRound(0, length);
-            return function (t) {
-                this.textContent = "stroke-dashoffset: " + i(t);
-            };
-        });
-};
+        .append("path")
+        .attr("d", "M0,-5L10,0L0,5");
 
 
-var xScaleFix = d3.scaleLinear().range([0, 210]);
-var yScaleFix = d3.scaleLinear().range([290, 0]);
-var lengthFix;
-var pathFix;
 
-d3.csv("examples/fixations_cover.csv", function (d) {
-    var lineFix = d3.line()
-        .x(function (d) {
-            return xScaleFix(d.norm_pos_x);
-        })
-        //        .x(function(d) { return d.x_norm })
-        .y(function (d) {
-            return yScaleFix(d.norm_pos_y);
-        });
-
-    var svgFix = d3.select("#svgFix");
-
-    pathFix = svgFix.append("path")
-        .datum(d)
-        .attr("id", "pathFix")
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr("d", lineFix)
-
-
-    lengthFix = pathFix.node().getTotalLength();
-
-    d3.select("#svgFix")
-        .selectAll('circle')
-        .data(d)
+    var edges = svg.append("g")
+        .selectAll("path")
+        .data(force.links())
         .enter()
-        .append('circle')
-        .attr('cx', function (d) {
-            return xScaleFix(d.norm_pos_x);
+        .append("path")
+        .attr("class", function (d) {
+            return "link " + d.type;
         })
-        .attr('cy', function (d) {
-            return yScaleFix(d.norm_pos_y);
-        })
-        .attr('r', 3);
-});
-
-// This function will animate the path over and over again
-function animateLineFixation() {
-    // Animate the path by setting the initial offset and dasharray and then transition the offset to 0
-    pathFix.attr("stroke-dasharray", lengthFix + " " + lengthFix)
-        .attr("stroke-dashoffset", lengthFix)
-        .transition()
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        .duration(6000)
-        .on("end", () => setTimeout(repeat, 1000)); // this will repeat the animation after waiting 1 second
-
-    // Animate the dashoffset changes
-    text.transition()
-        .duration(6000)
-        .tween("text", function (t) {
-            const i = d3.interpolateRound(0, lengthFix);
-            return function (t) {
-                this.textContent = "stroke-dashoffset: " + i(t);
-            };
+        .attr("marker-end", function(d) {
+            return "url(#" + d.type + ")";
         });
-};
 
-// we use a pie layout to render the circles
-// in case we want some spacing, we need to change
-// the value in the 'data' array
-const width = 400;
-const height = 400;
-const pieData = d3
-    .pie()
-    .sort(null)
-    .value((d) => {
-        return d.value;
-    })
+    var nodes = svg.append('g')
+        .selectAll('g')
+        .data(force.nodes())
+        .enter()
+        .append('g')
+        .call(force.drag);
 
-const svg = d3.create("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var colors = d3.scale.category20();
+    nodes.append('circle')
+        .attr({
+            r: 10,
+            fill: function (d, i) {
+                return colors(i);
+            },
+            stroke: 'black',
+            'stroke-width': 0
+        })
+        .call(force.drag()
+            .on("dragstart", function (d) {
+                d.fixed = true;
+                d3.select(this).attr('stroke-width', 3);
+            }))
+        .on('dblclick', function (d) {
+            d.fixed = false;
+            d3.select(this).attr('stroke-width', 0);
+        });
 
-const mainGroup = svg
-    .append("g")
-    .attr("id", "main")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    nodes.append('text')
+        .attr({
+            dx: 12,
+            dy: '.35em',
+            'pointer-events': 'none'
+        })
+        .style('font', '10px sans-serif')
+        .text(function (d) { return d.name });
 
-// Insert lines and circles groups, lines first so they are behind circles
-const linesGroup = mainGroup.append("g").attr("id", "lines");
-const circlesGroup = mainGroup.append("g").attr("id", "circles");
-
-const circles = [];
-for(let item of pieData) {
-    const [x, y] = arc.centroid(item);
-    circles.push({x, y});
-}
-
-// Draw circles using pie data and centroid method to get the center position
-circlesGroup
-    .selectAll("circle")
-    .data(circles, (_, index) => index)
-    .join((enter) => {
-        enter
-            .append("circle")
-            .attr("id", (_, index) => {
-                return `circle-${index}`;
-            })
-            .attr("r", 20)
-            .attr("cx", (d) => {
-                return d.x;
-            })
-            .attr("cy", (d) => {
-                return d.y;
-            })
-            .style("stroke-width", "2px")
-            .style("stroke", "#000")
-            .style("fill", "#963cff");
+    force.on("tick", function () {
+        edges.attr("d", function (d) {
+            var dx = d.target.x - d.source.x,
+                dy = d.target.y - d.source.y,
+                dr = Math.sqrt(dx * dx + dy * dy);
+            return "M" + d.source.x + "," + d.source.y + "A" +
+                dr + "," + dr + " 0 0,1 " +
+                d.target.x + "," + d.target.y;
+        });
+        nodes.attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
     });
 
-for (let line of lines) {
-    const fromCircle = circles[line.from];
-    const toCircle = circles[line.to];
-
-    const fromP = { x: fromCircle.x, y: fromCircle.y };
-    const toP = { x: toCircle.x, y: toCircle.y };
-
-    const path = d3.path();
-    path.moveTo(fromP.x, fromP.y);
-    path.quadraticCurveTo(0, 0, toP.x, toP.y);
-
-    linesGroup
-        .append("path")
-        .style("fill", "none")
-        .style("stroke-width", "2px")
-        .style("stroke-dasharray", "10 10")
-        .style("stroke", "#000")
-        .attr("d", path);
-}
-
+});
--- src/main/resources/static/codevisualization.js
+++ src/main/resources/static/codevisualization.js
@@ -1,104 +1,49 @@
 var svg = d3.select("svg"),
-    margin = 200,
-    width = svg.attr("width") - margin,
-    height = svg.attr("height") - margin;
-
-svg.append("text")
-    .attr("transform", "translate(100,0)")
-    .attr("x", 50)
-    .attr("y", 50)
-    .attr("font-size", "24px")
-    .text("Eye Gaze Fixation Durations")
-
-var x = d3.scaleBand().range([0, width]).padding(0.4),
-    y = d3.scaleLinear().range([height, 0]);
+    width = svg.attr("width"),
+    height = svg.attr("height"),
+    radius = Math.min(width, height) / 2;
 
 var g = svg.append("g")
-    .attr("transform", "translate(" + 100 + "," + 100 + ")");
-var dataCsv = d3.csv("examples/fixations.csv");
-dataCsv.then(function( data) {
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    x.domain(data.map(function(d) { return d.start_frame_index; }));
-    //x.domain([d3.min(data, function(d) { return d.start_frame_index; }), d3.max(data, function(d) { return d.start_frame_index; })]);
+var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
 
-    y.domain([0, d3.max(data, function(d) { return d.fixation_duration; })]);
-
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .append("text")
-        .attr("y", height - 250)
-        .attr("x", width - 100)
-        .attr("text-anchor", "end")
-        .attr("stroke", "black")
-        .text("Timestamp");
-
-    g.append("g")
-        .call(d3.axisLeft(y).tickFormat(function(d){
-            return d + "ms";
-        }).ticks(10))
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "-5.1em")
-        .attr("text-anchor", "end")
-        .attr("stroke", "black")
-        .text("Duration");
-
-    g.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .on("mouseover", onMouseOver) //Add listener for the mouseover event
-        .on("mouseout", onMouseOut)   //Add listener for the mouseout event
-        .attr("x", function(d) { return x(d.start_frame_index); })
-        .attr("y", function(d) { return y(d.fixation_duration); })
-        .attr("width", x.bandwidth())
-        .transition()
-        .ease(d3.easeLinear)
-        .duration(400)
-        .delay(function (d, i) {
-            return i * 50;
-        })
-        .attr("height", function(d) { return height - y(d.fixation_duration); });
+var pie = d3.pie().value(function(d) {
+    return (d.gaze_count/d.total_count)*100;
 });
 
-//mouseover event handler function
-function onMouseOver(d, i) {
-    d3.select(this).attr('class', 'highlight');
-    d3.select(this)
-        .transition()     // adds animation
-        .duration(400)
-        .attr('width', x.bandwidth() + 5)
-        .attr("y", function(d) { return y(d.fixation_duration) - 10; })
-        .attr("height", function(d) { return height - y(d.fixation_duration) + 10; });
+var path = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
 
-    g.append("text")
-        .attr('class', 'val')
-        .attr('x', function() {
-            return x(i.start_frame_index);
+var label = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(radius - 80);
+
+d3.csv("captures/threesurfaces/surfaces/normal_surface_gaze_distribution.csv", function(error, data) {
+    if (error) {
+        throw error;
+    }
+    var arc = g.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
+
+    arc.append("path")
+        .attr("d", path)
+        .attr("fill", function(d) { return color(d.data.surface_name); });
+
+    console.log(arc)
+
+    arc.append("text")
+        .attr("transform", function(d) {
+            return "translate(" + label.centroid(d) + ")";
         })
-        .attr('y', function() {
-            return y(i.fixation_duration + 15);
-        })
-        .text(function() {
-            return [ ' x: ' +i.norm_pos_x + ' y: ' +i.norm_pos_y];  // fixation_duration of the text
-        });
-}
+        .text(function(d) { return d.data.surface_name; });
+});
 
-//mouseout event handler function
-function onMouseOut(d, i) {
-    // use the text label class to remove label on mouseout
-    d3.select(this).attr('class', 'bar');
-    d3.select(this)
-        .transition()     // adds animation
-        .duration(400)
-        .attr('width', x.bandwidth())
-        .attr("y", function(d) { return y(d.fixation_duration); })
-        .attr("height", function(d) { return height - y(d.fixation_duration); });
-
-    d3.selectAll('.val')
-        .remove()
-}
-
-
+svg.append("g")
+    .attr("transform", "translate(" + (width / 2 - 120) + "," + 20 + ")")
+    .append("text")
+    .text("Gaze counts per Surface")
+    .attr("class", "title")
