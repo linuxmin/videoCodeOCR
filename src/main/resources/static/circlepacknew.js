05ff24b8bbe5033531289530f7e5eef17a4a541c
Benjamin Hartmann <extern.benjamin.hartmann@porscheinformatik.at>
2021-07-10 17:07:24 +0200

    first buggy version of adding links to inner nodes , links are not in right position

--- src/main/resources/static/circlepacknew.js
+++ src/main/resources/static/circlepacknew.js
@@ -50,43 +50,88 @@ function loadIt(error, data) {
 
 
     outerNodes.forEach(function (outerNode) {
+
         var innerData = outerNode.inner_nodes;
-        var innerLayout = d3.layout.force()
-            .size([40, 40])
-            .charge(-600)
-            .gravity(0.75)
-            .links([])
-            .nodes(innerData)
-            .on("tick", e => {
-                innerAnodes.attr("transform", function (d) {
-                    return "translate(" + (d.x - 20) + "," + (d.y - 20) + ")";
-                });
-            })
-            .start();
+        var inner_links = [];
+
+        innerData.forEach(function (innerLink) {
+            inner_links.push({
+                source: innerLink.source,
+                target: innerLink.target
+            });
+        })
+
+        var obj = {}
+
+        innerData.forEach(function (d, i) {
+            obj[d.id] = i;
+        })
+
+        inner_links.forEach(function (d) {
+            d.source = obj[d.source];
+            d.target = obj[d.target];
+        })
+
 
         var aNode = svg.select("g.outer#" + outerNode.id);
 
         var innerAnodes = aNode.selectAll("g.inner")
             .data(innerData, function (d) {
-                return d.id;
+                return d.numb;
             })
             .enter()
             .append("g")
             .attr("class", "inner")
             .attr("id", function (d) {
                 return d.id;
-            })
-            .call(innerLayout.drag()
-                .on("dragstart", function () {
-                    d3.event.sourceEvent.stopPropagation();
-                })
-            );
+            });
 
         innerAnodes
             .append("circle")
             .style("fill", "orange")
             .style("stroke", "blue")
             .attr("r", 6);
+
+        var innerLink = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(inner_links)
+            .enter().append("line");
+
+        var innerLayout = d3.layout.force()
+            .size([40, 40])
+            .charge(-600)
+            .gravity(0.75)
+            .links(inner_links)
+            .nodes(innerData)
+            .on("tick", innerTick)
+            .start();
+
+        innerAnodes.call(innerLayout.drag()
+            .on("dragstart", function () {
+                d3.event.sourceEvent.stopPropagation();
+            })
+        );
+
+        function innerTick(e) {
+            innerLink
+                .attr("x1", function (d) {
+                    return d.source.x;
+                })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+            innerAnodes.attr("transform", function (d) {
+                return "translate(" + (d.x - 20) + "," + (d.y - 20) + ")";
+            });
+        }
+
     })
 
     function outerTick(e) {
