4e78b112181dd0d2c5b9dc418a5adbbef404cd19
Benjamin Hartmann <extern.benjamin.hartmann@porscheinformatik.at>
2021-07-10 12:12:26 +0200

    refactored force directed circle packing example to be used with custom data

--- src/main/resources/static/circlepacknew.js
+++ src/main/resources/static/circlepacknew.js
@@ -1,14 +1,12 @@
-d3.json("class_network.json", loadIt);
+d3.json("testcircleforce.json", loadIt);
 
 
 var svg = d3.select("body").append("svg")
     .attr("width", 300)
     .attr("height", 300);
 
-////////////////////////
-// outer force layout
 function loadIt(error, data) {
-    var outerData = data;
+    var outerData = data.nodes;
 
     var outerLayout = d3.layout.force()
         .size([300, 300])
@@ -18,6 +16,7 @@ function loadIt(error, data) {
         .nodes(outerData)
         .on("tick", outerTick)
         .start();
+
 
     var outerNodes = svg.selectAll("g.outer")
         .data(outerData, function (d) {
@@ -39,78 +38,91 @@ function loadIt(error, data) {
 
 ////////////////////////
 // inner force layouts
+    // var innerData = [{"id": "A1"}, {"id": "A2"}];
 
-    var innerAdata = [{"id": "A1"}, {"id": "A2"}];
+    var innerAnodes;
 
-    var innerAlayout = d3.layout.force()
-        .size([40, 40])
-        .charge(-600)
-        .gravity(0.75)
-        .links([])
-        .nodes(innerAdata)
-        .on("tick", innerAtick)
-        .start();
+    function innerTick(e) {
+        innerAnodes.attr("transform", function (d) {
+            return "translate(" + (d.x - 20) + "," + (d.y - 20) + ")";
+        });
+    }
 
-    var aNode = svg.select("g.outer#A");
+    outerData.forEach(function (outerNode) {
+        var innerData = outerNode.inner_nodes;
+        var innerLayout = d3.layout.force()
+            .size([40, 40])
+            .charge(-600)
+            .gravity(0.75)
+            .links([])
+            .nodes(innerData)
+            .on("tick", innerTick)
+            .start();
 
-    var innerAnodes = aNode.selectAll("g.inner")
-        .data(innerAdata, function (d) {
-            return d.id;
-        })
-        .enter()
-        .append("g")
-        .attr("class", "inner")
-        .attr("id", function (d) {
-            return d.id;
-        })
-        .call(innerAlayout.drag()
-            .on("dragstart", function () {
-                d3.event.sourceEvent.stopPropagation();
+        var aNode = svg.select("g.outer#" + outerNode.id);
+
+        innerAnodes = aNode.selectAll("g.inner")
+            .data(innerData, function (d) {
+                return d.id;
             })
-        );
+            .enter()
+            .append("g")
+            .attr("class", "inner")
+            .attr("id", function (d) {
+                return d.id;
+            })
+            .call(innerLayout.drag()
+                .on("dragstart", function () {
+                    d3.event.sourceEvent.stopPropagation();
+                })
+            );
 
-    innerAnodes
-        .append("circle")
-        .style("fill", "orange")
-        .style("stroke", "blue")
-        .attr("r", 6);
+        innerAnodes
+            .append("circle")
+            .style("fill", "orange")
+            .style("stroke", "blue")
+            .attr("r", 6);
+    })
+
 
 ///////
 
-    var innerBdata = [{"id": "B1"}, {"id": "B2"}];
+    /*
+        var innerBdata = [{"id": "B1"}, {"id": "B2"}];
 
-    var innerBlayout = d3.layout.force()
-        .size([40, 40])
-        .charge(-600)
-        .gravity(0.75)
-        .links([])
-        .nodes(innerBdata)
-        .on("tick", innerBtick)
-        .start();
+        var innerBlayout = d3.layout.force()
+            .size([40, 40])
+            .charge(-600)
+            .gravity(0.75)
+            .links([])
+            .nodes(innerBdata)
+            .on("tick", innerBtick)
+            .start();
 
-    var bNode = svg.select("g.outer#B");
+        var bNode = svg.select("g.outer#B");
 
-    var innerBnodes = bNode.selectAll("g.inner")
-        .data(innerBdata, function (d) {
-            return d.id;
-        })
-        .enter()
-        .append("g")
-        .attr("class", "inner")
-        .attr("id", function (d) {
-            return d.id;
-        })
-        .call(innerBlayout.drag()
-            .on("dragstart", function () {
-                d3.event.sourceEvent.stopPropagation();
+        var innerBnodes = bNode.selectAll("g.inner")
+            .data(innerBdata, function (d) {
+                return d.id;
             })
-        );
+            .enter()
+            .append("g")
+            .attr("class", "inner")
+            .attr("id", function (d) {
+                return d.id;
+            })
+            .call(innerBlayout.drag()
+                .on("dragstart", function () {
+                    d3.event.sourceEvent.stopPropagation();
+                })
+            );
 
-    innerBnodes
-        .append("circle")
-        .style("fill", "orange")
-        .style("stroke", "blue")
-        .attr("r", 6);
+        innerBnodes
+            .append("circle")
+            .style("fill", "orange")
+            .style("stroke", "blue")
+            .attr("r", 6);
+    */
 
     //////////////////////////
     // functions
@@ -118,12 +130,6 @@ function loadIt(error, data) {
     function outerTick(e) {
         outerNodes.attr("transform", function (d) {
             return "translate(" + d.x + "," + d.y + ")";
-        });
-    }
-
-    function innerAtick(e) {
-        innerAnodes.attr("transform", function (d) {
-            return "translate(" + (d.x - 20) + "," + (d.y - 20) + ")";
         });
     }
 
