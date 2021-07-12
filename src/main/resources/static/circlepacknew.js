42b9e77e05e10da9cfe295f86773a38929508b84
Benjamin Hartmann <extern.benjamin.hartmann@porscheinformatik.at>
2021-07-12 16:53:37 +0200

    refactoring of circle pack new for better readability part 2

--- src/main/resources/static/circlepacknew.js
+++ src/main/resources/static/circlepacknew.js
@@ -6,6 +6,167 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
+function loadIt(error, data) {
+
+    var outerNodes = data.nodes
+    var outerLinks = data.links;
+
+    buildOuterGraph(outerNodes, outerLinks);
+    buildInnerGraph(outerNodes);
+    createPathMarkers();
+
+}
+
+function buildOuterGraph(outerNodes, outerLinks) {
+    buildGraphNodesAndLinks(outerNodes, outerLinks);
+    buildOuterLayout(outerNodes, outerLinks);
+}
+
+function buildInnerGraph(outerNodes) {
+    outerNodes.forEach(function (outerNode) {
+
+        var innerNodes = outerNode.inner_nodes;
+        var innerLinks = [];
+
+        innerNodes.forEach(function (innerLink) {
+            if (innerLink.source !== innerLink.target) {
+                innerLinks.push({
+                    source: innerLink.source,
+                    target: innerLink.target
+                });
+            }
+        })
+
+
+        buildGraphNodesAndLinks(innerNodes, innerLinks);
+        buildInnerLayout(outerNode, innerNodes, innerLinks);
+
+    })
+}
+
+function buildGraphNodesAndLinks(nodes, links) {
+    var tempObj = {}
+
+    nodes.forEach(function (d, i) {
+        tempObj[d.id] = i;
+    })
+
+    links.forEach(function (d) {
+        d.source = tempObj[d.source];
+        d.target = tempObj[d.target];
+    })
+}
+
+
+function buildOuterLayout(outerNodes, outerLinks) {
+    var outerLayout = d3.layout.force()
+        .size([width, height])
+        .charge(-80000)
+        .gravity(0.85)
+        .nodes(outerNodes)
+        .links(outerLinks)
+        .on("tick", () => {
+            outerPath.attr("d", function (d) {
+                var dx = d.target.x - d.source.x,
+                    dy = d.target.y - d.source.y,
+                    dr = Math.sqrt(dx * dx + dy * dy);
+                return "M" +
+                    d.source.x + "," +
+                    d.source.y + "A" +
+                    dr + "," + dr + " 0 0,1 " +
+                    d.target.x + "," +
+                    d.target.y;
+            });
+            outerNodesSelect.attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+        })
+        .start();
+
+    var outerPath = svg.append("svg:g").selectAll("path")
+        .data(outerLayout.links())
+        .enter().append("svg:path")
+        .attr("class", "link")
+        .attr("marker-end", "url(#outer)");
+
+
+    var outerNodesSelect = svg.selectAll("g.outer")
+        .data(outerNodes, function (d) {
+            return d.id;
+        })
+        .enter()
+        .append("g")
+        .attr("class", "outer")
+        .attr("id", function (d) {
+            return d.id;
+        })
+        .call(outerLayout.drag());
+
+    outerNodesSelect
+        .append("circle")
+        .style("fill", "pink")
+        .style("stroke", "blue")
+        .attr("r", 80);
+}
+
+function buildInnerLayout(outerNode, innerNodes, innerLinks) {
+    var outerNodeSelect = svg.select("g.outer#" + outerNode.id);
+
+    var innerNodesSelect = outerNodeSelect.selectAll("g.inner")
+        .data(innerNodes, function (d) {
+            return d.id;
+        })
+        .enter()
+        .append("g")
+        .attr("class", "inner")
+        .attr("id", function (d) {
+            return d.id;
+        });
+
+    var innerLayout = d3.layout.force()
+        .size([160, 160])
+        .charge(-600)
+        .gravity(1)
+        .links(innerLinks)
+        .nodes(innerNodes)
+        .on("tick", () => {
+
+            innerPath.attr("d", function (d) {
+                var dx = d.target.x - d.source.x,
+                    dy = d.target.y - d.source.y,
+                    dr = Math.sqrt(dx * dx + dy * dy);
+                return "M" +
+                    (d.source.x - 50) + "," +
+                    (d.source.y - 50) + "A" +
+                    dr + "," + dr + " 0 0,1 " +
+                    (d.target.x - 50) + "," +
+                    (d.target.y - 50);
+            });
+            innerNodesSelect.attr("transform", function (d) {
+                return "translate(" + (d.x - 50) + "," + (d.y - 50) + ")";
+            });
+        })
+        .start();
+
+    var innerPath = outerNodeSelect.append("svg:g").selectAll("path")
+        .data(innerLayout.links())
+        .enter().append("svg:path")
+        .attr("class", "link")
+        .attr("marker-end", "url(#inner)");
+
+    innerNodesSelect
+        .append("circle")
+        .style("fill", "orange")
+        .style("stroke", "blue")
+        .attr("r", 6);
+
+    innerNodesSelect.call(innerLayout.drag()
+        .on("dragstart", function () {
+            d3.event.sourceEvent.stopPropagation();
+        })
+    );
+}
+
 function createPathMarkers() {
     // build the arrow.
     svg.append("svg:defs").selectAll("marker")
@@ -33,162 +194,4 @@ function createPathMarkers() {
         .attr("orient", "auto")
         .append("svg:path")
         .attr("d", "M0,-5L10,0L0,5");
-}
-
-function loadIt(error, data) {
-
-    var outerNodes = data.nodes
-    var outerLinks = data.links;
-
-    buildGraphNodesAndLinks(outerNodes, outerLinks);
-
-
-    var outerLayout = d3.layout.force()
-        .size([width, height])
-        .charge(-80000)
-        .gravity(0.85)
-        .nodes(outerNodes)
-        .links(outerLinks)
-        .on("tick", outerTick)
-        .start();
-
-    createPathMarkers();
-
-// add the links and the arrows
-    var outerPath = svg.append("svg:g").selectAll("path")
-        .data(outerLayout.links())
-        .enter().append("svg:path")
-        .attr("class", "link")
-        .attr("marker-end", "url(#outer)");
-
-
-    var outerNodesSelect = svg.selectAll("g.outer")
-        .data(outerNodes, function (d) {
-            return d.id;
-        })
-        .enter()
-        .append("g")
-        .attr("class", "outer")
-        .attr("id", function (d) {
-            return d.id;
-        })
-        .call(outerLayout.drag());
-
-    outerNodesSelect
-        .append("circle")
-        .style("fill", "pink")
-        .style("stroke", "blue")
-        .attr("r", 80);
-
-
-    outerNodes.forEach(function (outerNode) {
-
-        var innerNodes = outerNode.inner_nodes;
-        var innerLinks = [];
-
-        innerNodes.forEach(function (innerLink) {
-            if (innerLink.source !== innerLink.target) {
-                innerLinks.push({
-                    source: innerLink.source,
-                    target: innerLink.target
-                });
-            }
-        })
-
-
-        buildGraphNodesAndLinks(innerNodes, innerLinks);
-
-
-        var outerNodeSelect = svg.select("g.outer#" + outerNode.id);
-
-        var innerNodesSelect = outerNodeSelect.selectAll("g.inner")
-            .data(innerNodes, function (d) {
-                return d.id;
-            })
-            .enter()
-            .append("g")
-            .attr("class", "inner")
-            .attr("id", function (d) {
-                return d.id;
-            });
-
-        innerNodesSelect
-            .append("circle")
-            .style("fill", "orange")
-            .style("stroke", "blue")
-            .attr("r", 6);
-
-
-        var innerLayout = d3.layout.force()
-            .size([160, 160])
-            .charge(-600)
-            .gravity(1)
-            .links(innerLinks)
-            .nodes(innerNodes)
-            .on("tick", innerTick)
-            .start();
-
-        var innerPath = outerNodeSelect.append("svg:g").selectAll("path")
-            .data(innerLayout.links())
-            .enter().append("svg:path")
-            .attr("class", "link")
-            .attr("marker-end", "url(#inner)");
-
-
-        innerNodesSelect.call(innerLayout.drag()
-            .on("dragstart", function () {
-                d3.event.sourceEvent.stopPropagation();
-            })
-        );
-
-        function innerTick(e) {
-
-            innerPath.attr("d", function (d) {
-                var dx = d.target.x - d.source.x,
-                    dy = d.target.y - d.source.y,
-                    dr = Math.sqrt(dx * dx + dy * dy);
-                return "M" +
-                    (d.source.x - 50) + "," +
-                    (d.source.y - 50) + "A" +
-                    dr + "," + dr + " 0 0,1 " +
-                    (d.target.x - 50) + "," +
-                    (d.target.y - 50);
-            });
-            innerNodesSelect.attr("transform", function (d) {
-                return "translate(" + (d.x - 50) + "," + (d.y - 50) + ")";
-            });
-        }
-
-    })
-
-    function outerTick(e) {
-        outerPath.attr("d", function (d) {
-            var dx = d.target.x - d.source.x,
-                dy = d.target.y - d.source.y,
-                dr = Math.sqrt(dx * dx + dy * dy);
-            return "M" +
-                d.source.x + "," +
-                d.source.y + "A" +
-                dr + "," + dr + " 0 0,1 " +
-                d.target.x + "," +
-                d.target.y;
-        });
-        outerNodesSelect.attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        });
-    }
-
-    function buildGraphNodesAndLinks(nodes, links) {
-        var tempObj = {}
-
-        nodes.forEach(function (d, i) {
-            tempObj[d.id] = i;
-        })
-
-        links.forEach(function (d) {
-            d.source = tempObj[d.source];
-            d.target = tempObj[d.target];
-        })
-    }
-
 }
